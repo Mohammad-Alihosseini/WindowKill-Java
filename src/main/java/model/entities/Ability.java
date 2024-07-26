4ee57cd7efcba8cda51377c8a1b281c831a46756
mohammad <malihosseini6@gmail.com>
2024-07-26 11:17:15 +0330

    Hypnos completed

--- src/main/java/model/entities/Ability.java
+++ src/main/java/model/entities/Ability.java
@@ -1,20 +1,53 @@
 package model.entities;
 
+import model.characters.Enemy;
 import model.characters.EpsilonModel;
-import model.movement.Movement;
+import model.characters.GeoShapeModel;
+import model.projectiles.EnemyBullet;
+import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 import java.awt.event.ActionListener;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 import static controller.constants.AbilityConstants.*;
 import static controller.constants.EntityConstants.EPSILON_SHOOTING_RAPIDITY;
+import static controller.constants.MovementConstants.*;
 import static model.collision.Collision.emitImpactWave;
 
 public enum Ability {
     HEPHAESTUS, ATHENA, APOLLO,
     DEIMOS, HYPNOS, PHONOI;
+    private static final Random random = new Random();
     private long lastSkillTime = 0;
+
+    private static void zeroVelocity(GeoShapeModel model) {
+        model.getMovement().setAngularSpeed(0.001F);
+        model.getMovement().setSpeedSave(0.001F);
+        model.getMovement().setSpeed(0.001F);
+        model.getMovement().setDeceleration(0.001F);
+        model.getMovement().setDecay(0.001F);
+    }
+
+    @NotNull
+    private static Timer HypnosTimer() {
+        Timer timer = new Timer((int) HYPNOS_ABILITY_TIME.getValue(), e1 -> {
+            for (GeoShapeModel model : GeoShapeModel.allShapeModelsList) {
+                if ((model instanceof Enemy || model instanceof EnemyBullet)) {
+                    model.getMovement().setAngularSpeed(random.nextFloat(-ANGULAR_SPEED_BOUND.getValue(), ANGULAR_SPEED_BOUND.getValue()));
+                    model.getMovement().setSpeedSave(DEFAULT_SPEED.getValue());
+                    model.getMovement().setSpeed(model.getMovement().getSpeedSave());
+                    model.getMovement().setDeceleration(DEFAULT_DECELERATION.getValue());
+                    model.getMovement().setDecay(DECELERATION_DECAY.getValue());
+                }
+            }
+        }
+        );
+
+        timer.setRepeats(false);
+        return timer;
+    }
 
     public String getName() {
         return switch (this) {
@@ -54,10 +87,12 @@ public enum Ability {
             case APOLLO -> e -> EpsilonModel.getINSTANCE().addHealth((int) APOLLO_ABILITY_HEALING_AMOUNT.getValue());
             case DEIMOS -> null;
             case HYPNOS -> e -> {
-                Movement.setHypnos(true);
-                Timer timer = new Timer((int) HYPNOS_ABILITY_TIME.getValue(), e1 -> Movement.setHypnos(false));
-
-                timer.setRepeats(false);
+                for (GeoShapeModel model : GeoShapeModel.allShapeModelsList) {
+                    if ((model instanceof Enemy || model instanceof EnemyBullet)) {
+                        zeroVelocity(model);
+                    }
+                }
+                Timer timer = HypnosTimer();
                 timer.start();
             };
             case PHONOI -> e -> {
